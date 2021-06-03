3a8feb05d3e0fa9320972b0dd7f4e684524a89b2
Alberto <crivealby@gmail.com>
2021-06-03 20:51:40 +0200

    using map

--- trainingrecords_cpp/training.h
+++ trainingrecords_cpp/training.h
@@ -2,23 +2,17 @@
 #define TRAINING_H
 #include <iostream>
 #include <QJsonObject>
+#include <map>
+
 class Training{
 private:
-    class Data{ //!TODO: to decide if mantain like this (extend or change completely)
-    public:
-        std::string pullup;
-        std::string pushup;
-        std::string squat;
-        std::string jumprope;
-        Data(std::string ="",std::string ="",std::string ="",std::string ="");
-
-    };
     unsigned int id;
-    std::string date;
-    Data info;
+    char* date;
+    std::map<char*,char*> info;
 
 public:
-    Training(unsigned int, std::string, std::string ="",std::string ="",std::string ="",std::string ="");
+    Training(unsigned int, std::string);
+    void addTraining("");
     void print() const;
     unsigned int getID() const;
     std::string getYear() const;
