921a6515558fcd8087759a09fc62ede7d88a3ec7
Alberto Crivellari <crivealby@gmail.com>
2022-06-04 14:22:21 +0200

    FEATURE starting signup window

---
+++ frontend/src/widgetView/wvSignup.cpp
@@ -0,0 +1,56 @@
+#include "../../widgetView/wvSignup.h"
+
+WV_Signup::WV_Signup(QWidget* p) : QWidget{p}{}
+
+/*
+ WV_Login::WV_Login(bool signup, QWidget * p) : QWidge{p} {
+    QVBoxLayout* layout;
+
+    QPushButton* sendButton;
+    QLabel* labelOther;
+
+    layout = new QVBoxLayout{this};
+    username = new QLineEdit{this};
+    password = new QLineEdit{this};
+    sendButton = new QPushButton{this};
+    labelOther = new QLabel{this};
+
+    username -> setPlaceholderText("Insert username");
+    password -> setPlaceholderText("Insert password");
+
+    if (signup) {
+        sendButton -> setText("SIGNUP");
+        labelOther -> setText("<a style= text-decoration:none; href=\"http://example.com/\">Login to your account</a>");
+
+        QLineEdit* name;
+        QLineEdit* surname;
+        name = new QLineEdit{this};
+        surname = new QLineEdit{this};
+
+        name -> setPlaceholderText("Insert name");
+        surname -> setPlaceholderText("Insert surname");
+        layout -> addWidget(name);
+        layout -> addWidget(surname);
+    }else {
+        sendButton -> setText("LOGIN");
+        labelOther -> setText("<a style= text-decoration:none; href=\"http://example.com/\">Create an account</a>");
+    }
+
+    layout -> addWidget(username);
+    layout -> addWidget(password);
+    layout -> addWidget(sendButton);
+    layout -> addWidget(labelOther);
+    setLayout(layout);
+
+    setWindowFlag(Qt::Window);
+    setWindowFlag(Qt::WindowMinMaxButtonsHint, false);
+    setWindowFlag(Qt::WindowCloseButtonHint, false);
+    setWindowModality(Qt::WindowModal);
+    setWindowTitle("Login");
+    resize(200,150);
+
+    QObject::connect(sendButton, &QPushButton::clicked, this, &WV_Login::sendLogin);
+    if (signup)  QObject::connect(&labelOther, &QLabel::linkActivated, this, &WV_Login::signup2Login);
+    else        QObject::connect(&labelOther, &QLabel::linkActivated, this, &WV_Login::login2Signup);
+} 
+ */
---
+++ frontend/widgetView/wvSignup.h
@@ -0,0 +1,13 @@
+#ifndef WVSIGNUP_H
+#define WVSIGNUP_H
+
+#include <QWidget>
+
+class WV_Signup : public QWidget {
+    Q_OBJECT
+private :
+public :
+    WV_Signup(QWidget* = nullptr);
+};
+
+#endif
