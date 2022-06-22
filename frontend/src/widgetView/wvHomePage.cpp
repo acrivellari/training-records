12efb2240e9df9a082e47de60c857296aabfe307
Alberto <crivealby@gmail.com>
2022-06-22 12:21:43 +0200

    BUGFIX fixed sd card error

--- frontend/src/widgetView/wvHomePage.cpp
+++ frontend/src/widgetView/wvHomePage.cpp
@@ -1,8 +1,14 @@
 #include "../../widgetView/wvHomePage.h"
 #include <QPushButton>
+#include <QLabel>
 #include <QHBoxLayout>
+#include <QVBoxLayout>
 
 WV_HomePage::WV_HomePage(QWidget* p) : QWidget{p} {
+	QVBoxLayout* mainLayout = new QVBoxLayout{this};
+	QHBoxLayout* menuBarLayout = new QHBoxLayout{this};
+	QPushButton* userButton = new QPushButton{"User"};
+	QLabel* iconLabel = new QLabel{"xd"};
 	QHBoxLayout* layout = new QHBoxLayout{this};
 	QPushButton* button = new QPushButton{"ciao"};
 	layout -> addWidget(button);
